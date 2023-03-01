f820d7a09ec78833caa4af1b171b1aaaa1f0859e
Maxboskp <105921497+Maxboskp@users.noreply.github.com>
2023-03-01 11:20:50 +0700

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "123456789";
-    $dbname = "product";
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);    
-?>
